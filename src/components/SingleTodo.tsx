3d64b9cd0cfb63bd7adf3404bb1ebe50362cf73d
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-12-29 18:47:11 +1000

    1. use react-icons;

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,11 @@
 // https://react-icons.github.io/react-icons/
 import React from 'react';
 import { Todo } from '../model';
+import { AiFillEdit } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { MdDownloadDone } from "react-icons/md";
+import "./styles.css";
+
 
 //   使用 interface
 // interface Props {
@@ -16,9 +21,16 @@ type Props = {
 }
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
-    return <div>
-
-    </div>;
+    return <form className="todos__single">
+        <span className="todos__single--text">
+            {todo.todo}
+        </span>
+        <div>
+            <span className='icon'> <AiFillEdit /> </span>
+            <span className='icon'> <MdDelete /></span>
+            <span className='icon'> <MdDownloadDone /></span>
+        </div>
+    </form>;
 
 }
 
