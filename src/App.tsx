6de1fdd8029bcbb50098aa99f1cdddb0d563a4e1
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-12-29 16:15:00 +1000

    1. define model; 2. add one para for InputField component;

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import React, { useState } from 'react';
 import './App.css';
 
 import InputField from './components/InputField';
+import { Todo } from './model';
 // 返回值类型 ：JSX.Element
 const App: React.FC = () => {
   // type of todo: <string>, value is     ""
   const [todo, setTodo] = useState<string>("");
+  // for debug
+  console.log(todo);
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAdd = () => { };
+
   // 使用 InputField 组件
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={function (): void {
+      }} />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,11 +5,14 @@ import "./styles.css"
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: () => void;
 };
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
     return <form action="" className="input">
         <input type="input" placeholder="Enter a Task"
             className="input__box"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
         />
         <button type="submit" className="input_submit">
             Go
