908d5bf56bf5a0dce80bebd74b6d501a97cadd1c
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-12-29 15:52:32 +1000

    1. define component InputField， InputField has 2 field， use interface define field type; 2. use InputField, pass parameter to InputField;

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
-import { log } from 'console';
+
+import InputField from './components/InputField';
 // 返回值类型 ：JSX.Element
 const App: React.FC = () => {
-
+  // type of todo: <string>, value is     ""
+  const [todo, setTodo] = useState<string>("");
+  // 使用 InputField 组件
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 }
 
-export default App;
+export default App;  
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,20 @@
 // rafce
 import React from 'react'
-
-const InputField = () => {
-    return (
-        <div>
-
-        </div>
-    )
+import "./styles.css"
+// 定义InputField 组件，添加属性及  属性类型
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+};
+const InputField = ({ todo, setTodo }: Props) => {
+    return <form action="" className="input">
+        <input type="input" placeholder="Enter a Task"
+            className="input__box"
+        />
+        <button type="submit" className="input_submit">
+            Go
+        </button>
+    </form>
 }
 
 export default InputField
